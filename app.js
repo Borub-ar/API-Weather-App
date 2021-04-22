fc2616e7107df0db4709db0d09d09bfaf8ccd7a9
Kacper <kacper.barabasz@wp.pl>
2021-04-22 22:26:36 +0200

    Add background image change based on Unsplash API and searched city name.

--- app.js
+++ app.js
@@ -7,29 +7,48 @@ toggleBtn.addEventListener('click', () => {
 });
 
 // Current weather call 
-window.addEventListener('keydown', async (e) => {
-    if (e.code === 'Enter') {
-        const inf = await currentWeather();
-        console.log(inf.data.data[0]);
-    };
-});
-
 const searchBtn = document.querySelector('.search_button')
 
 searchBtn.addEventListener('click', async () => {
-    const inf = await currentWeather();
-    console.log(inf.data.data[0]);
+    const cityInf = await currentWeather();
+    const bgPhoto = await bgImage();
+
+    const mainTemp = document.querySelector('.current_temp');
+    const cityName = document.querySelector('.main_city_name');
+    const currIcon = document.querySelector('.current_weather_icon');
+    const descr = document.querySelector('.weather_descr');
+    const sunrise = document.querySelector('.sunrise_span');
+    const sunset = document.querySelector('.sunset_span');
+    const cityPhoto = document.querySelector('.city_photo')
+
+
+    mainTemp.innerText = cityInf.temp;
+    cityName.innerText = cityInf.city_name;
+    currIcon.src = `https://www.weatherbit.io/static/img/icons/${cityInf.weather.icon}.png`
+    descr.innerText = cityInf.weather.description;
+    sunrise.innerText = cityInf.sunrise;
+    sunset.innerText = cityInf.sunset;
+    cityPhoto.style.backgroundImage = `url(${bgPhoto})`;
 });
 
-
-
+// Current weather in searched city function 
 const currentWeather = async () => {
     const searchCity = document.querySelector('#search').value;
 
     const config = { params: { city: searchCity, key: '4c1299c9ae164edd8cb6e247728e94af' } };
     const res = await axios.get('http://api.weatherbit.io/v2.0/current', config);
+    resData = res.data.data[0]
+    return resData
+};
 
-    return res
+// Current city background image function
+const bgImage = async () => {
+    const searchCity = document.querySelector('#search').value;
+
+    const config = { params: { client_id: 'hpthTi3lgM1vdtMHcfvAiW-hFUjaLHYCSAtG4y-Er-I', query: searchCity } };
+    const res = await axios.get('https://api.unsplash.com/search/photos', config);
+    resData = res.data.results[0].urls.full;
+    return resData
 };
 
 
