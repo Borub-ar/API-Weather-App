71e93a4faba70e2294795f239404ed067559d5d6
Kacper <barabasz64@gmail.com>
2021-04-27 19:04:57 +0200

    Add animations to weather cards.

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 'use strict';
 
-// //////////////////////////////////////////////////////////Aside toggle menu 
-const toggleBtn = document.querySelector('.toggle_btn');
-``
-toggleBtn.addEventListener('click', () => {
-    const asideNav = document.querySelector('.aside_nav');
-    asideNav.classList.toggle('aside_show');
-});
-
 // ///////////////////////////////////////////////////Weather request 
 const searchBtn = document.querySelector('.search_button');
 const searchCity = document.querySelector('#search');
 
 searchBtn.addEventListener('click', async () => {
+    menuAnimation();
     const cityInf = await currentWeather();
     // const bgPhoto = await bgImage();
+
     await sevenDayForecast();
+    
     const hForecast = await hourlyForecast();
     makeHourlyIcons(hForecast);
-
+    cardsAnimation();
+    
     searchCity.value = '';
 
     const mainTemp = document.querySelector('.current_temp');
@@ -29,7 +25,6 @@ searchBtn.addEventListener('click', async () => {
     const sunset = document.querySelector('.sunset_span');
     // const cityPhoto = document.querySelector('.city_photo');
 
-
     mainTemp.innerText = cityInf.temp;
     cityName.innerText = cityInf.city_name;
     currIcon.src = `https://www.weatherbit.io/static/img/icons/${cityInf.weather.icon}.png`;
@@ -39,7 +34,29 @@ searchBtn.addEventListener('click', async () => {
     // cityPhoto.style.backgroundImage = `url(${bgPhoto})`;
 });
 
-// //////////////Current weather in searched city - function 
+// /////////////////////////////Bottom menu unfolding animation
+const menuAnimation = () => {
+    if (searchCity.value) {
+        const searchBar = document.querySelector('.search');
+        const menu = document.querySelector('.bottom_menu');
+
+        menu.style.transform = 'scale(1, 1)';
+        searchBar.style.borderRadius = '0 0 0 0';
+    };
+};
+
+// /////////////////////////Seven days forecast cards animation
+const days = document.querySelectorAll('.day');
+
+const cardsAnimation = () => {
+    for (let i = 0; i < days.length; i++) {
+        setTimeout(() => {
+            days[i].style.transform = 'scale(1)';
+        }, i * 300)
+    };
+};
+
+// /////////////////Current weather in searched city - function 
 const currentWeather = async () => {
     try {
         const config = {
@@ -50,7 +67,7 @@ const currentWeather = async () => {
         };
 
         const res = await axios.get('http://api.weatherbit.io/v2.0/current', config);
-        const resData = res.data.data[0]
+        const resData = res.data.data[0];
         return resData
     } catch (err) {
         console.log(err);
@@ -97,11 +114,10 @@ const sevenDaysWeth = async () => {
 
 
 const sevenDayForecast = async () => {
-    const days = document.querySelectorAll('.day');
     const dayInfo = await sevenDaysWeth();
 
     for (let [index, day] of days.entries()) {
-        day.children[0].innerText = dayInfo[index + 1].datetime.substr(5).replace('-', '.')
+        day.children[0].innerText = dayInfo[index + 1].datetime.substr(5).replace('-', '.');
         day.children[1].src = `https://www.weatherbit.io/static/img/icons/${dayInfo[index + 1].weather.icon}.png`;
         day.children[2].children[0].innerText = dayInfo[index + 1].temp;
     };
@@ -119,7 +135,6 @@ const hourlyForecast = async () => {
         };
 
         const res = await axios.get('https://api.weatherbit.io/v2.0/forecast/hourly', config);
-        console.log(res.data.data);
         return res.data.data
     } catch (e) {
         console.log(e);
@@ -128,7 +143,6 @@ const hourlyForecast = async () => {
 
 
 const makeHourlyIcons = (dataAPI) => {
-
     for (const hour of dataAPI) {
 
         const weatherCard = document.createElement('div');
@@ -141,10 +155,11 @@ const makeHourlyIcons = (dataAPI) => {
         const weatherIcon = document.createElement('img');
         weatherIcon.src = `https://www.weatherbit.io/static/img/icons/${hour.weather.icon}.png`;
 
-        const weatherDescr = document.createElement('p');
-        weatherDescr.innerText = hour.weather.description;
+        const weatherTemp = document.createElement('p');
+        weatherTemp.classList.add('icon_temp');
+        weatherTemp.innerText = hour.temp;
 
-        weatherCard.append(datetime, weatherIcon, weatherDescr);
+        weatherCard.append(datetime, weatherIcon, weatherTemp);
     };
 };
 
@@ -152,14 +167,14 @@ const makeHourlyIcons = (dataAPI) => {
 const sevenDaysBtn = document.querySelector('.seven_days');
 const hourlyBtn = document.querySelector('.hours');
 const sevenDaysPanel = document.querySelector('.right');
-const hourForecastPanel = document.querySelector('.hourly_forecast')
+const hourForecastPanel = document.querySelector('.hourly_forecast');
 
 sevenDaysBtn.addEventListener('click', () => {
     sevenDaysPanel.style.transform = 'translate(0)';
-    hourForecastPanel.style.transform = 'translate(100%)';
+    hourForecastPanel.style.transform = 'translate(0, 100%)';
 });
 
 hourlyBtn.addEventListener('click', () => {
-    sevenDaysPanel.style.transform = 'translate(-200%)';
+    sevenDaysPanel.style.transform = 'translate(0, 400%)';
     hourForecastPanel.style.transform = 'translate(0)';
-})
+});
