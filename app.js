79620e0002632fc015531e0e70b3e3be6cce150e
Kacper <barabasz64@gmail.com>
2021-04-29 01:47:20 +0200

    Add basic responsiveness.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const searchCity = document.querySelector('#search');
 
 searchBtn.addEventListener('click', async () => {
     menuAnimation();
+
     const backgroundPhoto = await bgImage();
     const cityInf = await currentWeather();
     const hForecast = await hourlyForecast();
@@ -23,16 +24,16 @@ searchBtn.addEventListener('click', async () => {
 
 // /////////////////////////////Bottom menu unfolding animation
 const menuAnimation = () => {
-    if (searchCity.value) {
-        const searchBar = document.querySelector('.search');
-        const menu = document.querySelector('.bottom_menu');
+    const searchBar = document.querySelector('.search');
+    const menu = document.querySelector('.bottom_menu');
 
+    if (search.value) {
         menu.style.transform = 'scale(1, 1)';
         searchBar.style.borderRadius = '0 0 0 0';
-    };
+    }
 };
 
-// ///////////////Seven days forecast 
+// /////////////////////////////////////////Seven days forecast 
 const sevenDaysWeth = async () => {
     try {
         const config = {
@@ -68,10 +69,9 @@ const cardsAnimation = () => {
     for (let i = 0; i < days.length; i++) {
         setTimeout(() => {
             days[i].style.transform = 'scale(1)';
-        }, i * 300)
+        }, i * 300);
     };
 };
-
 
 // /////////////////Current weather in searched city - function 
 const currentWeather = async () => {
@@ -85,7 +85,6 @@ const currentWeather = async () => {
 
         const res = await axios.get('http://api.weatherbit.io/v2.0/current', config);
         const resData = res.data.data[0];
-        console.log(resData)
         return resData
     } catch (err) {
         console.log(err);
@@ -105,124 +104,11 @@ const bgImage = async () => {
 
         const res = await axios.get('https://api.unsplash.com/search/photos', config);
         const resData = res.data.results[0].urls.full;
-        console.log(resData)
         return resData
     } catch (err) {
-        console.log(err)
+        console.log(err);
     };
 };
-
-// ////////////////////////////////////////////////////////////////////////////Slides - background image
-
-const slideIn = (varImage, varInfo) => {
-    const container = document.querySelector('.image_container');
-
-    const cityImage = document.createElement('div');
-    cityImage.style.backgroundImage = `url(${varImage})`
-    cityImage.classList.add('city_photo', 'slide_right');
-
-
-
-
-
-
-
-
-    // Left panel
-    const leftPanel = document.createElement('div');
-    leftPanel.classList.add('city_info')
-
-    const temp = document.createElement('p');
-    temp.classList.add('temp')
-    temp.innerText = `${varInfo.temp}`;
-
-    const weatherIcon = document.createElement('img');
-    weatherIcon.classList.add('current_weather_icon');
-    weatherIcon.src = `https://www.weatherbit.io/static/img/icons/${varInfo.weather.icon}.png`;
-
-    const weatherDescr = document.createElement('p');
-    weatherDescr.classList.add('weather_descr');
-    weatherDescr.innerText = varInfo.weather.description;
-
-    const conFlex = document.createElement('div');
-    conFlex.classList.add('d_flex');
-
-    const sunRise = document.createElement('p')
-    sunRise.classList.add('sunrise');
-    sunRise.innerText = `Sunrise: ${varInfo.sunrise}`;
-
-    const sunSet = document.createElement('p');
-    sunSet.classList.add('sunset');
-    sunSet.innerText = `Sunset: ${varInfo.sunset}`;
-
-    conFlex.append(sunRise, sunSet);
-
-    leftPanel.append(temp, weatherIcon, weatherDescr, conFlex);
-
-
-
-
-    // Right panel 
-    const rightPanel = document.createElement('div');
-    rightPanel.classList.add('city_name')
-
-    const name = document.createElement('p');
-    name.innerText = varInfo.city_name;
-    rightPanel.append(name);
-
-
-
-    
-    cityImage.append(leftPanel, rightPanel)
-
-
-
-
-
-
-    container.append(cityImage);
-    cityImage.classList.add('slide_in')
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const slideOut = () => {
-    const slides = document.querySelectorAll('.city_photo');
-
-    for (let slide of slides) {
-        slide.classList.add('slide_out');
-    };
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // //////////////////////////Hourly forecast in searched city - function
 const hourlyForecast = async () => {
@@ -241,7 +127,6 @@ const hourlyForecast = async () => {
         console.log(e);
     };
 };
-
 
 const makeHourlyIcons = dataAPI => {
     for (const hour of dataAPI) {
@@ -263,7 +148,69 @@ const makeHourlyIcons = dataAPI => {
     };
 };
 
-// ///////////////////////////////////////////Toggle forecast type
+// /////////////////////////////////////////////////////////Making slides - function
+const slideIn = (varImage, varInfo) => {
+    const container = document.querySelector('.image_container');
+
+    const cityImage = document.createElement('div');
+    cityImage.style.backgroundImage = `url(${varImage})`;
+    cityImage.classList.add('city_photo', 'slide_right');
+
+    // Left panel
+    const leftPanel = document.createElement('div');
+    leftPanel.classList.add('city_info');
+
+    const temp = document.createElement('p');
+    temp.classList.add('temp');
+    temp.innerText = `${varInfo.temp}`;
+
+    const weatherIcon = document.createElement('img');
+    weatherIcon.classList.add('current_weather_icon');
+    weatherIcon.src = `https://www.weatherbit.io/static/img/icons/${varInfo.weather.icon}.png`;
+
+    const weatherDescr = document.createElement('p');
+    weatherDescr.classList.add('weather_descr');
+    weatherDescr.innerText = varInfo.weather.description;
+
+    const conFlex = document.createElement('div');
+    conFlex.classList.add('d_flex');
+
+    const sunRise = document.createElement('p');
+    sunRise.classList.add('sunrise');
+    sunRise.innerText = `Sunrise: ${varInfo.sunrise}`;
+
+    const sunSet = document.createElement('p');
+    sunSet.classList.add('sunset');
+    sunSet.innerText = `Sunset: ${varInfo.sunset}`;
+
+    conFlex.append(sunRise, sunSet);
+
+    leftPanel.append(temp, weatherIcon, weatherDescr, conFlex);
+
+    // Right panel 
+    const rightPanel = document.createElement('div');
+    rightPanel.classList.add('city_name');
+
+    const name = document.createElement('p');
+    name.innerText = varInfo.city_name;
+    rightPanel.append(name);
+
+    // Append both panels to cityImage
+    cityImage.append(leftPanel, rightPanel);
+
+    container.append(cityImage);
+    cityImage.classList.add('slide_in');
+};
+
+const slideOut = () => {
+    const slides = document.querySelectorAll('.city_photo');
+
+    for (let slide of slides) {
+        slide.classList.add('slide_out');
+    };
+};
+
+// /////////////////////////////////////////////////Toggle forecast type
 const sevenDaysBtn = document.querySelector('.seven_days');
 const hourlyBtn = document.querySelector('.hours');
 const sevenDaysPanel = document.querySelector('.right');
