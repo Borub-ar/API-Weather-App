8473881219ceae4a458732dfc3946e9e58353d26
Kacper <kacper.barabasz@wp.pl>
2021-09-27 17:41:11 +0200

    Add package.json

--- src/client/app.js
+++ src/client/app.js
@@ -1,15 +1,19 @@
 'use strict';
 
+// import 'core-js/stable';
+// import 'regenerator-runtime/runtime';
+// import './API-request';
+// import './carousel';
+
 const currentDate = document.querySelector('.date');
 const searchCity = document.querySelector('#search_input');
 const errorMessage = document.querySelector('.error_message');
 const sevenDaysPanel = document.querySelector('.seven_days_container');
-const days = document.querySelectorAll('.day');
 const hourForecastPanel = document.querySelector('.hourly_forecast');
 const menu = document.querySelector('.bottom_menu');
 const sevenDaysBtn = document.querySelector('.seven_days_switch');
 const hourlyBtn = document.querySelector('.hours_switch');
-const notLetters = document.querySelectorAll('.not_letter')
+const notLetters = document.querySelectorAll('.not_letter');
 
 // Set current date
 const date = new Date();
@@ -19,61 +23,73 @@ currentDate.textContent = today.toLocaleDateString();
 
 // Bottom menu unfolding animation
 const expandMenu = () => {
-    const searchBar = document.querySelector('.search_bar');
+  const searchBar = document.querySelector('.search_bar');
+  menu.style.display = 'flex';
+  menu.style.transform = 'scale(1)';
+  searchBar.style.borderRadius = '0';
+};
 
-    menu.style.display = 'flex';
-    menu.style.transform = 'scale(1)';
-    searchBar.style.borderRadius = '0';
-}
+const sevenDayForecast = APIData => {
+  sevenDaysPanel.innerHTML = '';
 
-const sevenDayForecast = async (APIData) => {
-    for (const [index, day] of days.entries()) {
-        const dateTimeData = APIData[index + 1].datetime.substr(5).replace('-', '.');
-        const dateTime = dateTimeData.startsWith(0) ? dateTimeData.substr(1) : dateTimeData;
+  APIData.forEach((day, i) => {
+    if (i !== 0) {
+      const dateTimeData = day.datetime.substr(5).replace('-', '.');
+      const dateTime = dateTimeData.startsWith(0) ? dateTimeData.substr(1) : dateTimeData;
+      const imgSource = `https://www.weatherbit.io/static/img/icons/${day.weather.icon}.png`;
 
-        day.children[0].textContent = dateTime;
-        day.children[1].src = `https://www.weatherbit.io/static/img/icons/${APIData[index + 1].weather.icon}.png`;
-        day.children[2].children[0].textContent = APIData[index + 1].temp;
+      const HTML = `
+            <div class="day">
+              <p>${dateTime}</p>
+              <img src="${imgSource}" alt="Temperature Icon" />
+              <p class="day_temp"><span>${day.temp}</span> ºC</p>
+            </div>
+      `;
+      sevenDaysPanel.insertAdjacentHTML('beforeend', HTML);
     }
-}
+  });
+  showSevenDaysIcon();
+};
 
 const removeNotification = () => {
-    notLetters.forEach(lett => {
-        lett.style.opacity = '0';
-    })
-}
+  notLetters.forEach(lett => {
+    lett.style.opacity = '0';
+  });
+};
 
 // Seven days forecast cards animation
 const showSevenDaysIcon = () => {
-    for (let i = 0; i < days.length; i++) {
-        setTimeout(() => {
-            days[i].style.transform = 'scale(1)';
-        }, i * 300);
-    }
-}
+  const days = document.querySelectorAll('.day');
+
+  for (let i = 0; i < days.length; i++) {
+    setTimeout(() => {
+      days[i].style.transform = 'scale(1)';
+    }, i * 300);
+  }
+};
 
 // Hourly forecast in searched city
 const makeHourlyIcons = dataAPI => {
-    hourForecastPanel.innerHTML = '';
+  hourForecastPanel.innerHTML = '';
 
-    for (const hour of dataAPI) {
-        const html = `
+  for (const hour of dataAPI) {
+    const html = `
         <div class="weather_card">
             <p>${hour.datetime.substr(11)}:00</p>
             <img src="https://www.weatherbit.io/static/img/icons/${hour.weather.icon}.png">
             <p class="day_temp">${hour.temp} ºC</p>
         </div>        
-        `
+        `;
 
-        hourForecastPanel.insertAdjacentHTML('beforeend', html)
-    }
-}
+    hourForecastPanel.insertAdjacentHTML('beforeend', html);
+  }
+};
 
-// Making slides 
+// Making slides
 const makeSlide = (APICityPhoto, APICityInfo) => {
-    const container = document.querySelector('.image_container');
+  const container = document.querySelector('.image_container');
 
-    const html = `
+  const html = `
     <div class="city_photo slide_right slide_in" style="background-image: url(${APICityPhoto})">
         <div class="city_info">
             <p class="temp">${APICityInfo.temp} ºC</p>
@@ -84,82 +100,76 @@ const makeSlide = (APICityPhoto, APICityInfo) => {
                 <p class="sunset">Sunrise: ${APICityInfo.sunset}</p>
             </div>
         </div>
-
         <div class="city_name">
             <p class="ct_name">${APICityInfo.city_name}</p>
         </div>
     </div>
     `;
 
-    container.insertAdjacentHTML('beforeend', html)
+  container.insertAdjacentHTML('beforeend', html);
 };
 
 const showButtons = () => {
-    sevenDaysBtn.style.transform = `scale(1)`;
-    hourlyBtn.style.transform = `scale(1)`;
-}
+  sevenDaysBtn.style.transform = `scale(1)`;
+  hourlyBtn.style.transform = `scale(1)`;
+};
 
 const pushSlidesLeft = () => {
-    const slides = document.querySelectorAll('.city_photo');
-    for (const slide of slides) slide.classList.add('slide_out');
-}
+  const slides = document.querySelectorAll('.city_photo');
+  slides.forEach(slide => slide.classList.add('slide_out'));
+};
 
 const showErrorMessage = () => {
-    errorMessage.classList.remove('hide_error');
-    errorMessage.classList.add('show_error');
-}
+  errorMessage.classList.remove('hide_error');
+  errorMessage.classList.add('show_error');
+};
 
 const hideErrorMessage = () => {
-    errorMessage.classList.remove('show_error');
-    errorMessage.classList.add('hide_error');
-}
+  errorMessage.classList.remove('show_error');
+  errorMessage.classList.add('hide_error');
+};
 
-// Weather request 
+// Weather request
 const makeRequest = async () => {
-    if (!searchCity.value) return
-    
-    const currentWeather = await getCurrentWeather();
+  if (!searchCity.value) return;
 
-    if (!currentWeather) return
+  const currentWeather = await getCurrentWeather();
 
-    removeNotification();
-    hideErrorMessage();
-    expandMenu();
+  if (!currentWeather) return;
 
-    const backgroundPhoto = await getBgImage();
-    const hourlyForecast = await getHourlyForecast();
-    const sevenForecast = await getSevenDaysWeather();
+  removeNotification();
+  hideErrorMessage();
+  expandMenu();
 
-    sevenDayForecast(sevenForecast);
-    updateDetaleInfo(sevenForecast);
+  const backgroundPhoto = await getBgImage();
+  // const hourlyForecast = await getHourlyForecast();
+  const sevenForecast = await getSevenDaysWeather();
 
-    makeHourlyIcons(hourlyForecast);
+  sevenDayForecast(sevenForecast);
+  updateDetaleInfo(sevenForecast);
 
-    showSevenDaysIcon();
-    showButtons();
+  // makeHourlyIcons(hourlyForecast);
+  showButtons();
+  pushSlidesLeft();
+  makeSlide(backgroundPhoto, currentWeather);
 
-    pushSlidesLeft();
-    makeSlide(backgroundPhoto, currentWeather);
-
-    searchCity.value = '';
-    searchCity.blur();
-
-}
+  searchCity.value = '';
+  searchCity.blur();
+};
 
 window.addEventListener('keydown', e => {
-    e.code === 'Enter' && makeRequest();
-})
+  e.code === 'Enter' && makeRequest();
+});
 
 document.querySelector('.search_button').addEventListener('click', makeRequest);
 
-
 // Toggle forecast type
 sevenDaysBtn.addEventListener('click', () => {
-    sevenDaysPanel.style.transform = 'translate(-50%)';
-    hourForecastPanel.style.transform = 'translate(0, 100%)';
-})
+  sevenDaysPanel.style.transform = 'translate(-50%)';
+  hourForecastPanel.style.transform = 'translate(0, 100%)';
+});
 
 hourlyBtn.addEventListener('click', () => {
-    sevenDaysPanel.style.transform = 'translate(-50%, 400%)';
-    hourForecastPanel.style.transform = 'translate(0)';
-})
+  sevenDaysPanel.style.transform = 'translate(-50%, 400%)';
+  hourForecastPanel.style.transform = 'translate(0)';
+});
--- src/client/carousel.js
+++ src/client/carousel.js
@@ -1,134 +1,132 @@
-'use strict';
-
 const blurLayer = document.querySelector('.blur_layer');
 const slidesPanel = document.querySelector('.slides_panel');
 const slides = document.querySelectorAll('.single_slide');
 const slideBtnsBox = document.querySelector('.slide_buttons');
 const slideBtns = document.querySelectorAll('.slide_btn');
-const closeBtn = document.querySelector('.close_btn')
+const closeBtn = document.querySelector('.close_btn');
 
 
 // Positioning slides
 const positionSlides = () => {
-    slides.forEach((slide, i) => slide.style.left = `${i * 100}%`);
-}
+  slides.forEach((slide, i) => (slide.style.left = `${i * 100}%`));
+};
 
 positionSlides();
 
 // Update slides informations
-const updateDetaleInfo = (apiData) => {
-    const slideInfo = document.querySelectorAll('.single_slide_info');
+const updateDetaleInfo = apiData => {
+  const slideInfo = document.querySelectorAll('.single_slide_info');
 
-    slideInfo.forEach((day, i) => {
-        day.children[0].textContent = apiData[i + 1].datetime.substr(5).replace('-', '.');
-        day.children[1].children[0].src = `https://www.weatherbit.io/static/img/icons/${apiData[i + 1].weather.icon}.png`;
-        day.children[2].textContent = apiData[i + 1].weather.description;
-        day.children[3].textContent = `${apiData[i + 1].temp} ºC`;
-        day.children[4].children[0].textContent = apiData[i + 1].clouds;
-        day.children[5].children[0].textContent = apiData[i + 1].vis;
-        day.children[6].children[0].textContent = apiData[i + 1].wind_cdir_full;
-        day.children[7].children[0].textContent = apiData[i + 1].wind_spd;
-        day.children[8].children[0].textContent = apiData[i + 1].pres;
-        day.children[9].children[0].textContent = apiData[i + 1].low_temp;
-        day.children[10].children[0].textContent = apiData[i + 1].high_temp;
-        day.children[11].children[0].textContent = apiData[i + 1].snow_depth;
-    })
-}
+  slideInfo.forEach((day, i) => {
+    day.children[0].textContent = apiData[i + 1].datetime.substr(5).replace('-', '.');
+    day.children[1].children[0].src = `https://www.weatherbit.io/static/img/icons/${apiData[i + 1].weather.icon}.png`;
+    day.children[2].textContent = apiData[i + 1].weather.description;
+    day.children[3].textContent = `${apiData[i + 1].temp} ºC`;
+    day.children[4].children[0].textContent = apiData[i + 1].clouds;
+    day.children[5].children[0].textContent = apiData[i + 1].vis;
+    day.children[6].children[0].textContent = apiData[i + 1].wind_cdir_full;
+    day.children[7].children[0].textContent = apiData[i + 1].wind_spd;
+    day.children[8].children[0].textContent = apiData[i + 1].pres;
+    day.children[9].children[0].textContent = apiData[i + 1].low_temp;
+    day.children[10].children[0].textContent = apiData[i + 1].high_temp;
+    day.children[11].children[0].textContent = apiData[i + 1].snow_depth;
+  });
+};
 
 // Show / hide - single day information panel
-const showPanel = (cardIndex) => {
-    blurLayer.style.display = 'block';
-    blurLayer.style.backgroundColor = 'rgba(0, 0, 0, 0.342)';
-    slidesPanel.style.width = '100%';
-    slideBtnsBox.style.display = 'flex';
+const showPanel = cardIndex => {
+  blurLayer.style.display = 'block';
+  blurLayer.style.backgroundColor = 'rgba(0, 0, 0, 0.342)';
+  slidesPanel.style.width = '100%';
+  slideBtnsBox.style.display = 'flex';
 
-    for (const child of slidesPanel.children) {
-        if (!child.classList.contains('close_btn')) {
-            child.style.transition = `opacity .2s ease-in-out, transform 1s ease-in-out`;
-        }
-        child.style.opacity = 1;
+  for (const child of slidesPanel.children) {
+    if (!child.classList.contains('close_btn')) {
+      child.style.transition = `opacity .2s ease-in-out, transform 1s ease-in-out`;
     }
+    child.style.opacity = 1;
+  }
 
-    // Moving to chosen day
-    for (const slide of slides) {
-        slide.classList.add('single_day');
-        slide.style.transform = `translate(-${cardIndex * 100}%)`;
-    }
-}
+  // Moving to chosen day
+  for (const slide of slides) {
+    slide.classList.add('single_day');
+    slide.style.transform = `translate(-${cardIndex * 100}%)`;
+  }
+};
 
 const hidePanel = () => {
-    for (const child of slidesPanel.children) {
-        child.style.opacity = 0;
-    }
+  for (const child of slidesPanel.children) {
+    child.style.opacity = 0;
+  }
 
-    slidesPanel.style.width = '0';
-    blurLayer.style.display = 'none';
-    slideBtnsBox.style.display = 'none';
-}
+  slidesPanel.style.width = '0';
+  blurLayer.style.display = 'none';
+  slideBtnsBox.style.display = 'none';
+};
 
+const moveSlide = slides => {
+  slides.forEach(slide => {
+    slide.style.transform = `translate(-${currentSlidePosition}%)`;
+  });
+};
 
 const moveSlideLeft = () => {
-    if (currentSlidePosition > -100) currentSlidePosition -= 100;
-    if (currentSlidePosition === -100) currentSlidePosition = 600;
+  if (currentSlidePosition > -100) currentSlidePosition -= 100;
+  if (currentSlidePosition === -100) currentSlidePosition = 600;
 
-    for (const slide of slides) {
-        slide.style.transform = `translate(-${currentSlidePosition}%)`;
-    }
+  moveSlide(slides);
 
-    setActiveBtn(currentSlidePosition);
-}
+  setActiveBtn(currentSlidePosition);
+};
 
 const moveSlideRight = () => {
-    if (currentSlidePosition < 700) currentSlidePosition += 100;
-    if (currentSlidePosition === 700) currentSlidePosition = 0;
+  if (currentSlidePosition < 700) currentSlidePosition += 100;
+  if (currentSlidePosition === 700) currentSlidePosition = 0;
 
+  moveSlide(slides);
 
-    for (const slide of slides) {
-        slide.style.transform = `translate(-${currentSlidePosition}%)`;
-    }
-
-    setActiveBtn(currentSlidePosition);
-}
+  setActiveBtn(currentSlidePosition);
+};
 
 let currentSlidePosition;
 
 // Add event listener to seven days forecast cards / click = show Panel
 for (const [index, day] of days.entries()) {
-    day.addEventListener('click', () => {
-        showPanel(index);
-        currentSlidePosition = index * 100;
+  day.addEventListener('click', () => {
+    showPanel(index);
+    currentSlidePosition = index * 100;
 
-        setActiveBtn(index * 100);
-    })
+    setActiveBtn(index * 100);
+  });
 }
 
 // Single day slide animation #1
 window.addEventListener('keydown', e => {
-    if (e.code === 'ArrowLeft') moveSlideLeft();
-    if (e.code === 'ArrowRight') moveSlideRight();
-})
+  if (e.code === 'ArrowLeft') moveSlideLeft();
+  if (e.code === 'ArrowRight') moveSlideRight();
+});
 
 // Single day slide animation #2
 slideBtnsBox.addEventListener('click', e => {
-    if (e.target.classList.contains('slide_btn')) {
-        slides.forEach(s => {
-            s.style.transform = `translate(-${e.target.dataset.slide * 100}%)`;
-            currentSlidePosition = e.target.dataset.slide * 100;
-        })
+  if (e.target.classList.contains('slide_btn')) {
+    slides.forEach(s => {
+      s.style.transform = `translate(-${e.target.dataset.slide * 100}%)`;
+      currentSlidePosition = e.target.dataset.slide * 100;
+    });
 
-        setActiveBtn(currentSlidePosition);
-    }
-})
+    setActiveBtn(currentSlidePosition);
+  }
+});
 
 const setActiveBtn = position => {
-    slideBtns.forEach(btn => {
-        btn.classList.remove('slide_btn_active')
-    })
+  slideBtns.forEach(btn => {
+    btn.classList.remove('slide_btn_active');
+  });
 
-    slideBtns[position / 100].classList.add('slide_btn_active');
-}
+  slideBtns[position / 100].classList.add('slide_btn_active');
+};
 
-// Single day slide animation #3 
+// Single day slide animation #3
 const slideBtnLeft = document.querySelector('.slide_left_btn');
 const slideBtnRight = document.querySelector('.slide_right_btn');
 
@@ -136,8 +134,8 @@ slideBtnLeft.addEventListener('click', moveSlideLeft);
 slideBtnRight.addEventListener('click', moveSlideRight);
 
 // hide information panel
-closeBtn.addEventListener('click', hidePanel)
+closeBtn.addEventListener('click', hidePanel);
 blurLayer.addEventListener('click', hidePanel);
 window.addEventListener('keydown', e => {
-    e.code === 'Escape' && hidePanel();
-})
+  e.code === 'Escape' && hidePanel();
+});
